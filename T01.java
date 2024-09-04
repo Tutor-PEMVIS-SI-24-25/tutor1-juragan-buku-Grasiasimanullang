2144207a9ce0b516103a9d5fe162f60419a6c1d6
Grasiasimanullang <grasiagayatrisimanullang@gmail.com>
2024-09-04 21:27:07 +0700

    Telah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,3 +1,17 @@
-// NIM - NAMA
-// NIM - NAMA
+// 12S24027 - Grasia Simanullang
+// 12S24027 - Grasia Simanullang
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String nama;
+
+        nama = input.nextLine();
+        System.out.println("grasia |");
+    }
+}
+
 
